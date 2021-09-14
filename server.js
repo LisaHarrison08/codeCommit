9681281ce128c7f5fb934e4727429a809c4bbd4f
Lisa Harrison <lmharrison0810@gmail.com>
2021-09-13 21:40:42 -0400

    notes included in the server js file

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
+// require() (import) the express module and create an express application
 const express = require('express');
-const path = require('path');
 const app = express();
+
+// The path module provides functionality to access and interact with the file system
+const path = require('path');
+
 
 // points to the public folder for static files
 app.use(express.static('public'));
 
-// if post request comes in html form or json
+/* The express.urlencoded() function is a built-in middleware function in Express. It parses incoming requests with urlencoded payloads and is based on body-parser.
+if post request comes in html form or json */
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
-// get request to the root url which points to the index.html page
+/* app.get() method specifies a callback function that will be invoked whenever there is an HTTP GET request with a path ('/') relative to the site root. The callback function takes a request and a response object as arguments.
+get request to the root url which points to the index.html page */
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
   });
 
-// localhost:3000 in browser (3000 is the port selected)
+// starts up the server on a specified port ('3000') localhost:3000 in browser 
 app.listen(3000);
